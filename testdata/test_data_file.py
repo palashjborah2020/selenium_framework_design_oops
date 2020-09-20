70ae5630b11c167f58eead25f7eea553367dedc5
palashjborah2020 <palashjborah@gmail.com>
2020-09-20 21:11:52 +0200

    All new files loaded for the first time into the github

---
+++ testdata/test_data_file.py
@@ -0,0 +1,10 @@
+browser = 'chrome'
+url = "https://www.google.co.in"
+search_value = "Selenium"
+wait_time = 20
+
+
+
+chrome_driver_path = "C:\\Users\\pborah\\PycharmProjects\\Selenium\\Selenium_Drivers\\Chromedriver-v85\\chromedriver.exe"
+firefox_driver_path = "E:\\driver\\geckodriver.exe"
+edge_driver_path = "E:\\driver\\msedgedriver.exe"
